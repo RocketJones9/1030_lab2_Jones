c52352ae01543fd952501ca9229c3a5327247c7a
RocketJones9 <illyana_jones@rockets.utoledo.edu>
2025-09-12 17:30:15 -0400

    Add EECS1030_lab2_Jones.java

---
+++ EECS1030_lab2_Jones.java
@@ -0,0 +1,10 @@
+public class EECS1030_lab2_Jones{
+
+    //Created by Illyana Jones on 2025 09 09
+
+    public static void main(String[] args) {
+        System.out.println ("Hello World!");
+    }
+}
+
+//This is a simple Java application that prints "Hello World!" to the console.
